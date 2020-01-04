eff724d99c6a6f1bcd4acd9f83889c1b7bca5c5c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-01-04 08:09:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,37 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+
+function addNewElementAsLi() {
+  let x = document.querySelector('ul');
+  let y = document.createElement('li');
+  let z = retrieveEmployeeInformation();
+  y.innerHTML = z;
+  x.appendChild(y);
+}
+
+
+function addNewLiOnClick() {
+  let x = document.querySelectorAll('input')[1];
+  x.addEventListener('click', addNewElementAsLi());
+  document.querySelector('input').value = ''
+}
+
+
+function clearEmployeeListOnLinkClick() {
+  let x = document.querySelector('ul');
+  // for (i = 0; i < x.length; i++) {
+  //   x.remove(x[i]);
+  // }
+  if (x.children.length > 0) {
+    
+  }
+}
